538979f9f6649cb440815dea4e2376d8ad2021df
Alaa Nasser <alaa.nasser024@gmail.com>
2018-05-15 12:21:44 +0200

    huff_code

--- Huff_code.hpp
+++ Huff_code.hpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+
 class Huffmancodes
 {
     struct Node
@@ -21,24 +27,54 @@ class Huffmancodes
             return (l->freq > r->freq);
         }
     };
-};
 
-void printCode(Node *root, std::string str, int &j)
+
+
+void printCode(Node *root, std::string str)
     {
         if (root == nullptr)
             return;
 
         if (root->data == '$')
         {
-            printCode(root->left, str + "0", j);
-            printCode(root->right, str + "1", j);
-        j++;
+            printCode(root->left, str + "0");
+            printCode(root->right, str + "1");
+        
         }
 
         if (root->data != '$')
         {
             std::cout << root->data << " : " << str << std::endl;
-            printCode(root->left, str + "0", j);
-            printCode(root->right, str + "1", j);
+            printCode(root->left, str + "0");
+            printCode(root->right, str + "1");
         }
     }
+
+ void HuffmanCodes(char data[], int freq[], int size)
+    {
+        struct Node *left, *right, *top;
+
+        std::priority_queue<Node *, std::vector<Node *>, compare> minHeap;
+
+        for (int i = 0; i < size; ++i)
+            minHeap.push(new Node(data[i], freq[i]));
+
+        while (minHeap.size() != 1)
+        {
+
+            left = minHeap.top();
+            minHeap.pop();
+
+            right = minHeap.top();
+            minHeap.pop();
+
+            top = new Node('$', left->freq + right->freq);
+
+            top->left = left;
+            top->right = right;
+
+            minHeap.push(top);
+        }
+        printCode(minHeap.top(), "");
+    }
+};
